8bc4c933fe43afc5cbea678fb30f08961957bf47
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-02-11 19:05:29 +0100

    add: CSS added

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,42 +1,67 @@
 <template>
-    <div class="profile-container">
-        <h2>Профиль</h2>
-        <div class="profile-info">
-            <p><strong>ID:</strong> {{ user.id }}</p>
-            <p><strong>Имя:</strong> {{ user.name }}</p>
-            <p><strong>Выполнено задач:</strong> {{ user.completedTasks }}</p>
-        </div>
+  <div class="profile-container">
+    <h2>Профиль</h2>
+    <div class="profile-info">
+      <p><strong>ID:</strong> {{ user.id }}</p>
+      <p><strong>Имя:</strong> {{ user.name }}</p>
+      <p><strong>Выполнено задач:</strong> {{ user.completedTasks }}</p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ProfileView',
-    data() {
-        return {
-            user: {
-                id: '',
-                name: '',
-                completedTasks: 0
-            }
-        }
+  name: "ProfileView",
+  data() {
+    return {
+      user: {
+        id: "",
+        name: "",
+        completedTasks: 0,
+      },
+    };
+  },
+  async mounted() {
+    await this.fetchProfile();
+  },
+  methods: {
+    async fetchProfile() {
+      try {
+        const tg_user = window.Telegram.WebApp.initDataUnsafe?.user;
+        const response = await fetch(
+          `https://orange-sniffle-4xr5qq5g69wfq4wq-8000.app.github.dev/api/main/${tg_user.id}`
+        );
+        const data = await response.json();
+        this.user.id = tg_user.id;
+        this.user.name = tg_user.name;
+        this.user.completedTasks = data.completedTasks;
+      } catch (error) {
+        console.log("error", error);
+      }
     },
-    async mounted() {
-        await this.fetchProfile()
-    },
-    methods: {
-        async fetchProfile() {
-            try {
-                const tg_user = window.Telegram.WebApp.initDataUnsafe?.user
-                const response = await fetch(`https://orange-sniffle-4xr5qq5g69wfq4wq-8000.app.github.dev/api/main/${tg_user.id}`)
-                const data = await response.json()
-                this.user.id = tg_user.id
-                this.user.name = tg_user.name
-                this.user.completedTasks = data.completedTasks
-            } catch (error) {
-                console.log('error', error)
-            }
-        }
-    }
-}
+  },
+};
 </script>
+
+<style>
+.profile-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 16px;
+}
+
+.profile-info {
+  background-color: #ffffffcc;
+  backdrop-filter: blur(8px);
+  padding: 16px;
+  border-radius: 8px;
+  text-align: left;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+  margin-top: 16px;
+  width: 100%;
+  max-width: 320px;
+}
+</style>
