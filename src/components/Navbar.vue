f394f757720846df7fc4b2af38fc11e977a26dd9
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-02-12 15:26:41 +0100

    update: Navbar updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,31 @@
 <template>
-    <nav class="navbar">
-        <router-link to="/" class="nav-link">Задачи</router-link>
-        <router-link to="/profile" class="nav-link">Профиль</router-link>
-    </nav>
+  <nav class="navbar">
+    <router-link to="/" class="nav-link">Задачи</router-link>
+    <router-link to="/profile" class="nav-link">Профиль</router-link>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: "Navbar"
-}
+  name: "Navbar",
+};
 </script>
 
-<style>
+<style scoped>
 .navbar {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    height: 60px;
-    background-color: rgba(255, 255, 255, 0.8);
-    backdrop-filter: blur(10px);
-    border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 60px;
+  background-color: rgba(255, 255, 255, 0.8);
+  backdrop-filter: blur(10px);
+  border-top: 1px solid #ccc;
 }
 
 .nav-link {
-    text-decoration: none;
-    color: #333;
-    font-size: 16px;
-    font-weight: 500;
+  text-decoration: none;
+  color: #333;
+  font-size: 16px;
+  font-weight: 500;
 }
 </style>
